add84a7c9f92bf3cce1115360e12ce6dc0f7ef26
BWBrooke <68975512+BWBrooke@users.noreply.github.com>
2021-04-26 15:35:52 -0500

    Update Configuration.h
    Update Machine Name

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -201,7 +201,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "JellyBOX"
+#define CUSTOM_MACHINE_NAME "JellyBOX 2 DE"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
